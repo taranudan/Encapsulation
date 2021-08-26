f01e9deb854007f67c52be56703909b7100e5d41
taranudan <88153465+taranudan@users.noreply.github.com>
2021-08-26 19:20:22 +0300

    Update BigCup.java

--- BigCup.java
+++ BigCup.java
@@ -9,11 +9,11 @@ public class BigCup extends Cup {
 	}
 	
 	public void setLiquidVolume(int liquidVolume) {			
-    	if (liquidVolume>= 0 && liquidVolume <= 1000) {
-    		this.liquidVolume = liquidVolume;
-    	}
-    	else {
-    		System.err.println ("The data entered for liquidVolume is incorrect");
-    	}
+    		if (liquidVolume>= 0 && liquidVolume <= 1000) {
+    			this.liquidVolume = liquidVolume;
+    		}
+    		else {
+    			System.err.println ("The data entered for liquidVolume is incorrect");
+    		}
 	}
 }
